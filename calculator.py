0213595ed962a8184c1dc86b59403cbcd9bd188f
calipho <106757959+calipho@users.noreply.github.com>
2022-07-04 18:48:15 +0300

    done

--- calculator.py
+++ calculator.py
@@ -1,6 +1,19 @@
+##Enter the first number: 4
+##Enter the second number: 6
+##Choose the operation (+, -, /, *): *
+##The answer is 24
+
 
 def main():
-	#write your code here
+	firstnumber = input("Enter the first number: ")
+	print(firstnumber.isdigit())
+	firstnumber = int(firstnumber)
+	secondnumber = input("Enter the second number: ")
+	print(secondnumber.isdigit())
+	secondnumber = int(secondnumber)
+	operation = input("Choose the operation (+, -, /, *): ")
+	if operation == "*":
+		print("The answer is", firstnumber * secondnumber)
 	pass
 	
 
